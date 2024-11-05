86824270b121ae9071fb4f4d5a03362285b98e41
Ayda Sholani <ayda.sholani@gmail.com>
2024-11-05 17:39:39 +0100

    refactor

--- app/ocr/text_recognition.py
+++ app/ocr/text_recognition.py
@@ -1,18 +1,29 @@
 import easyocr
 import os
-import difflib
+from difflib import get_close_matches
+from fuzzywuzzy import fuzz
+from spellchecker import SpellChecker
 
 def load_word_list():
     """Load the word list from a file."""
-    file_path = os.path.join(os.getcwd(), "app", "ocr", 'codenames-swe-eng.txt')
+    file_path = os.path.join(os.getcwd(), "app", "ocr", 'codenames-swedish.txt')
     with open(file_path, 'r', encoding='utf-8') as file:
         word_list = {line.strip().upper() for line in file} 
     return word_list
   
 def check_word_in_list(word):
-    """Check if the cleaned text matches any word in the word list."""
+    """Check if the cleaned text matches any word in the word list, or suggest closest match."""
     word_list = load_word_list()
-    return word.upper() in word_list
+
+    if word in word_list:
+        print("Exakt matchning hittades: ", word)
+        return word
+ 
+    close_matches = get_close_matches(word, word_list, n=1, cutoff=0.6)  
+
+    best_match = close_matches[0].upper() if close_matches else None
+    print(f"Original word: {word}, Suggested match: {best_match}")
+    return best_match
 
 def initialize_easyocr(lang_list=['sv', 'en']):
     """Initializes the EasyOCR reader."""
@@ -26,7 +37,7 @@ def read_text_from_textbox(reader, textbox):
             decoder='wordbeamsearch', 
             beamWidth=20, 
             allowlist=allowlist,
-            min_size=5
+            min_size=4
     )
     
     for (bbox, text, prob) in results:
@@ -36,24 +47,24 @@ def read_text_from_textbox(reader, textbox):
 def recognize_text(image, boxes, min_words=25):
     """Recognizes text in each specified bounding box in the image, filters recognized words, and checks for minimum word count."""
     correct_words = []
- 
-    reader = initialize_easyocr()
+    reader = initialize_easyocr()  # Initialisera OCR-läsaren en gång
+    
     for (x1, y1, x2, y2) in boxes:
         # Beskär textboxen
         textbox = image[y1:y2, x1:x2]
         
         # Kör OCR på det beskurna textområdet
         results = read_text_from_textbox(reader, textbox)
-        
         # Extrahera och filtrera text
         for text in results:
-            recognized_text = text[1].strip()  # Extrahera och trimma
-            if recognized_text and check_word_in_list(recognized_text):  # Kontrollera ordlistan
-                correct_words.append(recognized_text.upper())  # Lägg till ordet om det är giltigt
+            recognized_text = text[1].strip().upper()  
+            corrected_word = check_word_in_list(recognized_text)
+    
+            # Lägg till det korrigerade ordet om det finns en matchning
+            if corrected_word:
+                correct_words.append(corrected_word)
+            else:
+                correct_words.append("Null") 
                 
-    # Kontrollera om vi har tillräckligt många korrekta ord
-    if len(correct_words) < min_words:
-        print(f"För få korrekta ord. Endast {len(correct_words)} identifierade.")
-        return []  # Returnera tom lista om vi inte har tillräckligt många korrekta ord
 
     return correct_words
